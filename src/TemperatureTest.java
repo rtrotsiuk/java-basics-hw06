e73acfcafa1c72cc782001da12ed4022447bf333
Roman Trotsiuk <trotsyuk.roman@gmail.com>
2020-04-12 15:52:37 +0300

    added test data & Temperature methods

--- src/TemperatureTest.java
+++ src/TemperatureTest.java
@@ -14,8 +14,9 @@ public class TemperatureTest {
     return new Object[][]{
         {212.0, 100.0},
         {32.0, 0.0},
-        {-15, -26.11111111111111}
-        // TODO add 2 more test data here
+        {-15, -26.11111111111111},
+        {140.0, 60.0},
+        {14.0, -10.0}
     };
   }
 
@@ -28,8 +29,9 @@ public class TemperatureTest {
     return new Object[][]{
         {100.0, 212.0},
         {0.0, 32.0},
-        {-26.11111111111111, -15}
-        // TODO add 2 more test data here
+        {-26.11111111111111, -15},
+        {60.0, 140.0},
+        {-10.0, 14.0}
     };
   }
 
